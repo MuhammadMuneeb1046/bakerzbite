3193841881b5b1f67cc1bd7b4a52aae6e5ff890b
MuhammadMuneeb1046 <140684309+MuhammadMuneeb1046@users.noreply.github.com>
2023-09-05 22:25:44 +0500

    Add files via upload

---
+++ Comparison Page/comparison.js
@@ -0,0 +1,50 @@
+function product_1(){
+    var proname= document.getElementById("pro_1").value;
+    var detail_1= document.getElementById("detail_1");
+
+    if (proname=="Iphone 6"){
+        document.getElementById("img_1").src="img/iphone/6.jpg";
+        detail_1.innerHTML="<li>Mobille Name: Iphone 6</li><li>Camera: 4MP</li><li>RAM: 2GB</li>"
+    }
+    else if (proname=="Iphone 7"){
+        document.getElementById("img_1").src="img/iphone/7.jpg";
+        detail_1.innerHTML="<li>Mobille Name: Iphone 6</li><li>Camera: 4MP</li><li>RAM: 2GB</li>"
+    }
+    else if (proname=="Iphone 7 Plus"){
+        document.getElementById("img_1").src="img/iphone/7 plus.jpg";
+        detail_1.innerHTML="<li>Mobille Name: Iphone 6</li><li>Camera: 4MP</li><li>RAM: 2GB</li>"
+    }
+    else if (proname=="Iphone X"){
+        document.getElementById("img_1").src="img/iphone/x.jpg";
+        detail_1.innerHTML="<li>Mobille Name: Iphone 6</li><li>Camera: 4MP</li><li>RAM: 2GB</li>"
+    }
+    else if (proname=="Iphone Xs"){
+        document.getElementById("img_1").src="img/iphone/xs.jpg";
+        detail_1.innerHTML="<li>Mobille Name: Iphone 6</li><li>Camera: 4MP</li><li>RAM: 2GB</li>"
+    }
+}
+function product_2(){
+    var proname= document.getElementById("pro_2").value;
+    var detail_1= document.getElementById("detail_2");
+
+    if (proname=="Iphone 6"){
+        document.getElementById("img_2").src="img/iphone/6.jpg";
+        detail_1.innerHTML="<li>Mobille Name: Iphone 6</li><li>Camera: 4MP</li><li>RAM: 2GB</li>"
+    }
+    else if (proname=="Iphone 7"){
+        document.getElementById("img_2").src="img/iphone/7.jpg";
+        detail_1.innerHTML="<li>Mobille Name: Iphone 6</li><li>Camera: 4MP</li><li>RAM: 2GB</li>"
+    }
+    else if (proname=="Iphone 7 Plus"){
+        document.getElementById("img_2").src="img/iphone/7 plus.jpg";
+        detail_1.innerHTML="<li>Mobille Name: Iphone 6</li><li>Camera: 4MP</li><li>RAM: 2GB</li>"
+    }
+    else if (proname=="Iphone X"){
+        document.getElementById("img_2").src="img/iphone/x.jpg";
+        detail_1.innerHTML="<li>Mobille Name: Iphone 6</li><li>Camera: 4MP</li><li>RAM: 2GB</li>"
+    }
+    else if (proname=="Iphone Xs"){
+        document.getElementById("img_2").src="img/iphone/xs.jpg";
+        detail_1.innerHTML="<li>Mobille Name: Iphone 6</li><li>Camera: 4MP</li><li>RAM: 2GB</li>"
+    }
+}
